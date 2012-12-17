64724d0f27d671a9f03c98c9f9e0e66fab8e5ea3
hongmk900 <khm_815@hanmail.net>
2012-12-17 16:44:09 +0900

    add source

---
+++ gugudan/src/uos/Gugudan.java
@@ -0,0 +1,18 @@
+package uos;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		for (int i = 2; i < 10; i++) {
+			printDan(i);
+		}
+
+	}
+
+	public static void printDan(int i) {
+		for (int j = 1; j < 10; j++) {
+			System.out.println(i + "x" + j + "=" + i * j);
+		}
+	}
+
+}
